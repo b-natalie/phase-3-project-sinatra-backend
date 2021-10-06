2d79023604ea5d569ae3a29013ac0f5f47a42102
Natalie Barba <nbarba2010@gmail.com>
2021-10-06 11:45:35 -0700

    natalie: updated services_controller.rb

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,7 +1,13 @@
 class ServicesController < ApplicationController
 
     get "/services" do
-        Service.all.to_json
+        # Service.all.to_json
+        services_array = Service.all.map do |service|
+            service_json = service.as_json
+            service_json[:client_count] = service.clients.count
+            service_json
+        end
+        services_array.to_json
     end
 
     post "/services" do
@@ -11,6 +17,9 @@ class ServicesController < ApplicationController
 
     get "/services/:id" do
         service = Service.find(params[:id])
+        # service_json = service.as_json
+        # service_json[:client_count] = service.clients.count
+        # service_json.to_json
         service.to_json
     end
 
