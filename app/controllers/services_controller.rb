2e3ee0cba01e93fb601c1622beb3ae7f9da7bbf7
Natalie Barba <nbarba2010@gmail.com>
2021-10-07 08:20:55 -0700

    natalie: add client list to services

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -15,13 +15,31 @@ class ServicesController < ApplicationController
         service.to_json
     end
 
-    get "/services/:id" do
-        service = Service.find(params[:id])
+    # get "/services/:id" do
+    #     service = Service.find(params[:id])
         # service_json = service.as_json
         # service_json[:client_count] = service.clients.count
         # service_json.to_json
-        service.to_json
+    #     service.to_json
+    # end
+
+    # service.clients.first.reservations.find_by(service_id: service.id)
+
+    get "/services/:id" do
+        service = Service.find(params[:id])
+        service_json = service.as_json
+        clients = service.clients.all
+        clientsResId = clients.map do |client|
+            client_json = client.as_json  
+            res = client.reservations.find_by(service_id: service.id)
+            res_id = res.id
+            client_json[:res_id] = res_id
+            client_json
+        end
+        service_json[:clients] = clientsResId
+        service_json.to_json
     end
+
 
     patch "/services/:id" do
         service = Service.find(params[:id])
